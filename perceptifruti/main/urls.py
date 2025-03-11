816e8c61c2c12c43bf4cd118eba36a6e200d471f
Romero Cartaxo <romero.cartaxo@solyd.com.br>
2025-03-10 22:08:40 -0300

    feat: add endpoint to send webcam image

--- perceptifruti/main/urls.py
+++ perceptifruti/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('receive-webcam-image/', views.receive_webcam_image, name='receive_webcam_image'),
 ]
--- perceptifruti/main/views.py
+++ perceptifruti/main/views.py
@@ -1,6 +1,19 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from PIL import Image
+from io import BytesIO
+import base64
 
 
 def home(request):
     return render(request, 'camera.html')
+
+
+def receive_webcam_image(request):
+    image_b64 = request.POST['image'].split(',')[1]
+    image_pillow = Image.open(BytesIO(base64.b64decode(image_b64)))
+    # Saves image
+    # image_pillow.save('webcam.png')
+    return HttpResponse(status=200)
