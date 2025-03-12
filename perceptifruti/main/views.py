c9e24a851aa0c989161fff2f69bb01481f5c30b7
Romero Cartaxo <romero.cartaxo@solyd.com.br>
2025-03-11 21:28:58 -0300

    feat: Receive new image through endpoint

--- perceptifruti/main/views.py
+++ perceptifruti/main/views.py
@@ -1,7 +1,8 @@
-import json
+import os
 
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from PIL import Image
 from io import BytesIO
 import base64
@@ -12,8 +13,16 @@ def home(request):
 
 
 def receive_webcam_image(request):
-    image_b64 = request.POST['image'].split(',')[1]
-    image_pillow = Image.open(BytesIO(base64.b64decode(image_b64)))
-    # Saves image
-    # image_pillow.save('webcam.png')
-    return HttpResponse(status=200)
+    input_image_b64 = request.POST['image'].split(',')[1]
+    image_pillow = Image.open(BytesIO(base64.b64decode(input_image_b64)))
+    example_image = os.path.join(settings.STATICFILES_DIRS[0], 'img/example.png')
+    image_extension = os.path.splitext(example_image)[1][1:]
+
+    output_image_b64 = ''
+    with open(example_image, 'rb') as f:
+        output_image_b64 = base64.b64encode(f.read()).decode('utf-8')
+
+    output_image_b64 = f'data:image/{image_extension};base64,{output_image_b64}'
+    print(output_image_b64)
+
+    return JsonResponse({'image': output_image_b64})
